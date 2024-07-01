45d23c408bf15e48ff667d4829ae89837ee8c707
Erik Wei <erikwei@sas.upenn.edu>
2024-07-01 02:26:41 -0700

    ABOUT ME updates

--- src/utils/bin/about.ts
+++ src/utils/bin/about.ts
@@ -5,12 +5,12 @@ const about = async (args: string[]): Promise<string> => {
            ▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▄                 -----------
         ▄▓▓▀ ▄▓▓▀▓▓▓▀▓▓▄ ▀▀▓▓▄               ABOUT ME
       ▓▓▀  ▄▓▀   ▐▓▓  ▀▓▓    ▓▓▄             ${config.name}
-    ▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓            <u><a href="${config.resume_url}" target="_blank">resume</a></u>
-   ▓▓     ▓▓▓    ▐▓▓    ▐▓▓     ▓▓          爵 <u><a href="${config.repo}" target="_blank">Github repo</a></u>
-▐▓▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▓       爵 I'm a rising senior at the University of Pennsylvania studying Computer Science, and am 
-▐▓                                 ▐▓         currently a SWE intern at Coinbase working on distributed streaming for transactions. I enjoy  
-▐▓        > E R I K   W E I        ▐▓         exploring new frameworks to create beautiful and functional software. Outside of CS, I play
-▐▓                                 ▐▓         tennis, produce lofi music (try the 'youtube' command!), and watching great TV shows.
+    ▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓            <u><a href="${config.resume_url}" target="_blank">Resume</a></u>
+   ▓▓     ▓▓▓    ▐▓▓    ▐▓▓     ▓▓          爵 <u><a href="${config.repo}" target="_blank">Github Repo</a></u>
+▐▓▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▓       爵 I'm a rising senior at the University of Pennsylvania studying Computer Science, and currently a
+▐▓                                 ▐▓         SWE intern at Coinbase working on distributed streaming for core ledger transactions. I'm most
+▐▓        > E R I K   W E I        ▐▓         interested in systems programming and building scalable backend infrastructure. Outside of CS, 
+▐▓                                 ▐▓         I enjoy playing tennis, producing lofi (try the 'youtube' command), and watching great TV shows!
 ▐▓▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▓       ----------- 
    ▓▓      ▐▓▓    ▓▓    ▐▓▓     ▓▓           CONTACT 
     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓            <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
